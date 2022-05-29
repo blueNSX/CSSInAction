1ed1a305028f0722e4d6289e387998d4d4135be4
blueNSX <evantage@comcast.net>
2022-05-29 13:40:02 -0400

    052922

---
+++ MaxCSSFinal_052622/_vti_cnf/shared.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 Mar 2018 22:15:16 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|EVANTAGE\\Administrator
+vti_modifiedby:SR|EVANTAGE\\Administrator
+vti_timecreated:TR|15 Mar 2018 22:15:16 -0000
+vti_cacheddtm:TX|15 Mar 2018 22:15:16 -0000
+vti_filesize:IR|2043
+vti_backlinkinfo:VX|MyFormR/index.html MyFormR/start-hosting/index.html MyFormR/packages/index.html MyFormR/xxxindex.html
